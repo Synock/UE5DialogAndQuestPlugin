53b439b2ebf834f791529579b13e6fa2704761d4
Synock <maximilienguislain@gmail.com>
2022-08-07 23:48:33 +0200

    Added a way to toggle quest journal widget

--- Source/DialogAndQuestPlugin/Public/Interfaces/DialogDisplayInterface.h
+++ Source/DialogAndQuestPlugin/Public/Interfaces/DialogDisplayInterface.h
@@ -34,6 +34,9 @@ public:
 
 	//this function is intended to process custom script within dialog lines, for instance to retrieve player name or whatever
 	virtual FString ProcessScriptedFunction(const FString& InputString, UWorld* WorldContext) const;
+
+	UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
+	void ToggleQuestJournal();
 	//----------------------------------------------------------------------------------------------------------------------
 
 	//UFUNCTION(BlueprintCallable, BlueprintImplementableEvent)
